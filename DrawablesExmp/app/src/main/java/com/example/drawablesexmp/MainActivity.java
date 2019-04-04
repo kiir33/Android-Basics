9e7e48b494943c715cbc6e602b7d422b163f3048
kiir33 <kiranbanmala3@gmail.com>
2019-04-04 17:35:29 +0545

    myapp add

--- DrawablesExmp/app/src/main/java/com/example/drawablesexmp/MainActivity.java
+++ DrawablesExmp/app/src/main/java/com/example/drawablesexmp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.drawablesexmp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.create_new :
+                Toast.makeText(getApplicationContext(),"create new selected",Toast.LENGTH_SHORT).show();
+
+            case R.id.open:
+                Toast.makeText(getApplicationContext(),"open selected",Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
     }
 }
