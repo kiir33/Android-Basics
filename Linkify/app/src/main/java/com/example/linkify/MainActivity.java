6739500f32d5e8859e9bf5f319b8728ae55c4d73
kiir33 <kiranbanmala3@gmail.com>
2019-04-05 14:03:40 +0545

    linkify

--- Linkify/app/src/main/java/com/example/linkify/MainActivity.java
+++ Linkify/app/src/main/java/com/example/linkify/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.linkify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv1, tv2, tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.textView);
+        tv2 = findViewById(R.id.textView2);
+        tv3 = findViewById(R.id.textView3);
+
+        Linkify.addLinks(tv1, Linkify.WEB_URLS);
+        Linkify.addLinks(tv2,Linkify.PHONE_NUMBERS);
+        Linkify.addLinks(tv3,Linkify.EMAIL_ADDRESSES);
     }
 }
