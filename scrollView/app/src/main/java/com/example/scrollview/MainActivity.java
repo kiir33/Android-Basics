56b4261e4028d1d5bdbaa01ad0c166ff4bcf8760
kiir33 <kiranbanmala3@gmail.com>
2019-04-05 12:51:26 +0545

    day 3 activities

---
+++ scrollView/app/src/main/java/com/example/scrollview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.scrollview;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setMessage("You are about to close this App! Do you want to continue?").
+                        setCancelable(false).
+                        setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog =alert.create();
+                alertDialog.setTitle("Alert !!!");
+                alertDialog.show();
+            }
+        });
+    }
+
+
+}
